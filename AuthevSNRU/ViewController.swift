4053fabfc0c9a1b86d0cc65486793230b0810802
Apinya <Apinya@abc.com>
2019-03-20 14:08:37 +0700

    Get Event From Click Success

--- AuthevSNRU/ViewController.swift
+++ AuthevSNRU/ViewController.swift
@@ -12,12 +12,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }//Main Method
+
+    
+    @IBAction func rogisterButton(_ sender: UIButton) {
+        
+        print("You Click Register")
+        
     }
+    
 
 
-}
+
+}//Main Class
 
