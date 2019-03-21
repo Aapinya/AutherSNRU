8470c42c3f365be670f23f58e26b749783be738e
Apinya <Apinya@abc.com>
2019-03-21 16:20:41 +0700

    Register Sussess

--- AuthevSNRU/RegisterViewController.swift
+++ AuthevSNRU/RegisterViewController.swift
@@ -53,7 +53,7 @@ class RegisterViewController: UIViewController {
     if (name.count == 0) || (user.count == 0) || (password.count == 0) {
         myAlert(titleString: "Have Space", messageString: "Please Fill All Blank")
     }else{
-        
+        uploadData(name: name, user: user, password: password)
     }
     
     }//upload Button
@@ -77,6 +77,34 @@ class RegisterViewController: UIViewController {
     func uploadData(name: String,user:String,password: String) -> Void {
         
         let urlString: String = "https://www.androidthai.in.th/snru/addDataApiya.php?isAdd=true&Name=\(name)&User=\(name)r&Password=\(password)"
+        
+        
+        let  url = URL(string: urlString)
+        let  request = NSMutableURLRequest(url: url!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest){data, response, error in
+            
+            if error !=  nil {
+                print("Error")
+            }  else  {
+                
+                if let testData =  data {
+                    
+                    let canReadData = NSString(data: testData, encoding: String.Encoding.utf8.rawValue )
+                    print("canRead ==> \(String(describing: canReadData))")
+                }
+            } //if
+            
+        }  // end Task
+        task.resume()
+        
+       
+                    
+        
+        
+        
+       
+        
+        
         
         
         
