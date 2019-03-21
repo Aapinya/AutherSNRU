f34ca31e0ba0806478ae59da482069cb65c4dd5e
Apinya <Apinya@abc.com>
2019-03-21 11:29:24 +0700

    Check Space Success

--- AuthevSNRU/RegisterViewController.swift
+++ AuthevSNRU/RegisterViewController.swift
@@ -40,12 +40,25 @@ class RegisterViewController: UIViewController {
     print("You Click Upioad")
     
     let name = nameTextField.text!
+    let user = userTextField.text!
+    let password = passwordTextField.text!
+    
     
     
     
     print("name =\(name)")
+    print("user =\(user)")
+    print("password=\(password)")
     
+    if (name.count == 0) || (user.count == 0) || (password.count == 0) {
+        myAlert(titleString: "Have Space", messageString: "Please Fill All Blank")
+    }else{
+        
+    }
     
+    }//upload Button
+    func myAlert(titleString: String,messageString: String) -> Void {
+        print("title=\(titleString),message=\(messageString)")
     }
     
     
